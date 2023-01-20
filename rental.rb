0ecd41a081408e96af67e6ac12a17b9519d89296
Selma <selmandapanda@yahoo.com>
2023-01-20 12:34:39 +0200

    Solve merging conflict

--- rental.rb
+++ rental.rb
@@ -5,19 +5,11 @@ class Rental
   def initialize(person, book, date)
     @date = date
 
-<<<<<<< HEAD
     @person = person
     person&.rentals&.push(self)
 
     @book = book
     book&.rentals&.push(self)
-=======
-    @book = book
-    book.rentals << self
-
-    @person = person
-    person.rentals << self
->>>>>>> b8e832aa4d7b001051e2e761a21f0fd3d54e1171
   end
 
   def to_json(*args)
--- teacher.rb
+++ teacher.rb
@@ -3,13 +3,8 @@ require_relative './person'
 class Teacher < Person
   attr_reader :specialization
 
-<<<<<<< HEAD
   def initialize(name, age, specialization, id = Random.rand(1..1000))
     super(name, age, id)
-=======
-  def initialize(specialization, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
->>>>>>> b8e832aa4d7b001051e2e761a21f0fd3d54e1171
     @specialization = specialization
   end
 
