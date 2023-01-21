331c6f5e7ad793b548feaf3ca0fa58309937dfb0
Selma <selmandapanda@yahoo.com>
2023-01-21 20:26:22 +0200

    Modify the decorator design pattern

---
+++ test/decorator_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../decorator'
+require_relative '../nameable'
+require_relative '../person'
+
+describe Decorator do
+    before :each do
+      @new_person = Person.new(20, 'selma')
+      @new_decorator = Decorator.new(@new_person)
+    end
+  
+    it 'Decorator should should inherit from Nameable' do
+      expect(@new_decorator).to be_kind_of(Nameable)
+    end
+  
+    it 'Nameable should be an instance of Person' do
+      expect(@new_decorator.nameable).to be_instance_of(Person)
+    end
+  
+    it 'correct_name should return name attribute from Person' do
+      expect(@new_decorator.correct_name).to eql('selma')
+    end
+  end
+  
